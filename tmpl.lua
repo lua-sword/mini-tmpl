9389835932ab620a398302918bc175e92e879865
TsT <tst2005@gmail.com>
2017-10-11 21:25:16 +0200

    allow to add custom ast handler

--- tmpl.lua
+++ tmpl.lua
@@ -37,29 +37,39 @@ local function render(ast, values)
 	if type(ast)=="string" then
 		return ast
 	end
-	if type(ast)=="table" and ast.tag=="mark" then -- mark
-		local k = assert(ast[1])
-		local v2 = values[k]
-		assert(v2, "no value found for "..tostring(k))
-		for _n=1,10 do -- while v3 is a template (max 1000 recursions)
-			v2 = render(v2, values)
-			if type(v2)~="table" then
-				break
-			end
-			if v2.tag~="template" then
-				error("resolved is still not a valid value")
-			end
+	if type(ast)=="table" and type(ast.tag)=="string" then
+		local f = M.ast[ast.tag]
+		if not f then
+			error("no handler for ast type "..ast.tag)
 		end
-		assert(type(v2)=="string", "v2 is not a string ?!")
-		return v2
-	elseif type(ast)=="table" and ast.tag=="template" then
-		local r = {}
-		for _i, a in ipairs(ast) do
-			table.insert(r, render(a, values))
-		end
-		return table.concat(r,"")
+		return f(ast, values)
 	end
 	error("ast invalid type, must be a table(template|mark) or a string, got "..type(ast))
 end
 M.render=render
+
+M.ast = {}
+M.ast["template"] = function(ast, values)
+	local r = {}
+	for _i, a in ipairs(ast) do
+		table.insert(r, render(a, values))
+	end
+	return table.concat(r,"")
+end
+M.ast["mark"] = function(ast, values)
+	local k = assert(ast[1])
+	local v2 = values[k]
+	assert(v2, "no value found for "..tostring(k))
+	for _n=1,10 do -- while v3 is a template (max 1000 recursions)
+		v2 = render(v2, values)
+		if type(v2)~="table" then
+			break
+		end
+		if v2.tag~="template" then
+			error("resolved is still not a valid value")
+		end
+	end
+	assert(type(v2)=="string", "v2 is not a string ?!")
+	return v2
+end
 return M
