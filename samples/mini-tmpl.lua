3a25fb8a4e95623dd9db3acaa1b625c3ba830135
TsT <tst2005@gmail.com>
2020-11-05 12:53:05 +0100

    tmpl.render_mod and templ.prepare_mod

--- samples/mini-tmpl.lua
+++ samples/mini-tmpl.lua
@@ -2,7 +2,10 @@
 local M = {}
 M._VERSION = "mini-tmpl 0.3.0"
 
-M.prepare = require "mini-tmpl.prepare".prepare
-M.render = require "mini-tmpl.render".render
+M.prepare_mod = require "mini-tmpl.prepare"
+M.prepare = M.prepare_mod.prepare
+
+M.render_mod = require "mini-tmpl.render"
+M.render= M.render_mod.render
 
 return M
