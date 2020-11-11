443b22bc6cdb5f2cd91b0753f61467b3ff40a58e
TsT <tst2005@gmail.com>
2020-11-11 21:18:00 +0100

    sample fixed

--- samples/subst1.lua
+++ samples/subst1.lua
@@ -2,7 +2,7 @@ local tmpl = require "mini-tmpl"
 
 local template = tmpl.prepare "Hello !{1}."
 local data = {"John"}
-x = tmpl.render(template, data) -- 
+x = tmpl.render({template}, data)
 
 -- similar to
 y = string.format("Hello %s.", "John")
@@ -10,7 +10,7 @@ y = string.format("Hello %s.", "John")
 local function tmpl_format(t, ...)
 	local fmt = tmpl.prepare(t)
 	local data = {...}
-	return tmpl.render(template, data)
+	return tmpl.render({template}, data)
 end
 z = tmpl_format("Hello !{1}.", "John")
 
