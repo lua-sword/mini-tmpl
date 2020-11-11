f06df8d61dcde20b56da0e2c066a85a56f0f65ce
TsT <tst2005@gmail.com>
2020-11-11 21:22:21 +0100

    sample fixed

--- test.tmpl.lua
+++ test.tmpl.lua
@@ -20,11 +20,11 @@ txt.bar = [[!{z} !{w}]]
 do
 	local t = tmpl.prepare(txt.foo)
 	local t2 = tmpl.prepare(txt.bar)
-	local v2 = tmpl.render(t2, {
+	local v2 = tmpl.render({t2}, {
 		z="Z",
 		w="WORLD",
 	})
-	local R = tmpl.render(t, {
+	local R = tmpl.render({t}, {
 		"ME",
 		v2,
 		["q"]="'",
@@ -33,9 +33,10 @@ do
 end
 
 do
+--[[ -- test disabled : template into data is not allowed !
 	local t = tmpl.prepare(txt.foo)
 	local t2 = tmpl.prepare(txt.bar)
-	local R = tmpl.render(t, {
+	local R = tmpl.render({t}, {
 		"ME",
 		t2,
 		["q"]="'",
@@ -43,5 +44,6 @@ do
 		w="WORLD",
 	})
 	assert(R==[['I am ME Hello Z WORLD']])
+]]--
 end
 print("ok")
