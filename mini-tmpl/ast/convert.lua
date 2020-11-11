9a421d343610def19b406970f4cedaa29ca85fb5
TsT <tst2005@gmail.com>
2020-11-11 19:21:37 +0100

    remove ast/convert

--- mini-tmpl/ast/convert.lua
+++
@@ -1,5 +0,0 @@
-
--- function(templates, values, functions, config)
--- args = { templates, values, functions, {dynamicfield="DYN", main=1})
---AST[const.Convert] = function(ast, parent, current) -- +functions
---end
