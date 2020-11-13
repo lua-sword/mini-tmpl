9122cb5a4666d3e0b4bfd43d62cba2268e5d3b13
TsT <tst2005@gmail.com>
2020-11-14 00:32:02 +0100

    no gett/getf

--- mini-tmpl/common.lua
+++ mini-tmpl/common.lua
@@ -6,13 +6,15 @@ local const = {"TEMPLATE","INCLUDE","LOOP","VAR","IF","PIPE", "EVAL"}
 for i,name in ipairs(const) do
 	const[name]=i
 end
-local const_internal = {"GETF", "GETT"}
 
+--[[
+local const_internal = {"GETF", "GETT"}
 local offset=100
 for i, name in ipairs(const_internal) do
 	const[i+offset] = name
 	const[name] = i+offset
 end
+]]--
 
 M.const = const
 
